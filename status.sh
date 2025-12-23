e6ebbdc67385a92911f5655aae7364ca00b48f93
arjorb <arjorb@gmail.com>
2025-12-23 10:44:19 +0200

    Improve the script to use multiple functions

--- status.sh
+++ status.sh
@@ -5,8 +5,36 @@ server_name=$(hostname)
 function memoryCheck(){
     echo ""
         echo "The current memory usage on ${server_name} is: "
-        echo ""
-        top -l 1 -s 0 | head -n 10
-        echo ""
+        check_disk_usage
+        check_cpu_usage
+        check_memory_usage
+        check_kernel_version
 }
+
+function check_disk_usage(){
+    echo "Current Disk usage: "
+    df -h
+    echo ""
+}
+
+function check_cpu_usage(){
+    echo "Current CPU usage: "
+    top -l 1 | grep "CPU usage"
+    echo ""
+
+}
+
+function check_memory_usage(){
+    echo "Current RAM usage: "
+    top -l 1 | grep PhysMem
+    echo ""
+
+}
+
+function check_kernel_version(){
+    echo "Exact Kernel version: "
+    uname -r
+    echo ""
+}
+
 memoryCheck
